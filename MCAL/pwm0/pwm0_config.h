04a3c5f4e2575d6c10ca52d9ecce33d9354f42d7
Ahmad <Ahmedabdellatef3846@gmail.com>
2025-03-01 23:24:07 +0200

    add pwm0

---
+++ MCAL/pwm0/pwm0_config.h
@@ -0,0 +1,16 @@
+/*
+ * pwm0_config.h
+ *
+ * Created: 3/1/2025 10:22:06 PM
+ *  Author: Ahmad abdullatif
+ */
+
+
+#ifndef PWM0_CONFIG_H_
+#define PWM0_CONFIG_H_
+
+#define PWM0_MODE PWM0_NON_INVERTING
+
+
+
+#endif /* PWM0_CONFIG_H_ */
