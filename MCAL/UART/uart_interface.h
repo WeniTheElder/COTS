9f91d5e6172fba9e1eb1bb2e569c10f22085947e
Ahmad <Ahmedabdellatef3846@gmail.com>
2025-03-08 03:57:06 +0200

    add UART module

---
+++ MCAL/UART/uart_interface.h
@@ -0,0 +1,23 @@
+/*
+ * uart_interface.h
+ *
+ * Created: 3/8/2025 7:48:04 AM
+ *  Author: Ahmad abdullatif
+ */
+
+
+#ifndef UART_INTERFACE_H_
+#define UART_INTERFACE_H_
+
+#define UBRR_VALUE 103
+
+void UART_voidInit    (void);
+void UART_voidTxChar  (u8 copy_u8TxData);
+void UART_voidRxChar  (u8* copy_pu8RxData);
+void UART_voidTxString(u8* copy_pu8String);
+void UART_voidRxString(u8* copy_pu8String);
+
+
+
+
+#endif /* UART_INTERFACE_H_ */
